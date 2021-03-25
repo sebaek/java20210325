f026cab000c005192f63d2f457b716ff9bbed2bd
sebaek <sebaek@gmail.com>
2021-03-25 16:36:04 +0900

    char

--- src/p02/lecture/A07TypeCharEx1.java
+++ src/p02/lecture/A07TypeCharEx1.java
@@ -8,9 +8,9 @@ public class A07TypeCharEx1 {
 		for (int i = 0; i < str.length(); i++) {
 			char c = str.charAt(i);
 			
-//			if ((0x61 <= c && c <= 0x7a) || (0x41 <= c && c < 0x5a)) {
-//			if ((97 <= c && c <= 122) || (65 <= c && c < 90)) {
-			if (('a' <= c && c <= 'z') || ('A' <= c && c < 'Z')) {
+//			if ((0x61 <= c && c <= 0x7a) || (0x41 <= c && c <= 0x5a)) {
+//			if ((97 <= c && c <= 122) || (65 <= c && c <= 90)) {
+			if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')) {
 				count++;
 			}
 		}
