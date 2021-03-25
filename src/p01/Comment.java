752bbcc3ee1962cfc7834860c0caee700a81bb62
sebaek <sebaek@gmail.com>
2021-03-25 10:28:28 +0900

    first commit

---
+++ src/p01/Comment.java
@@ -0,0 +1,18 @@
+package p01;
+
+public class Comment {
+	public static void main(String[] args) {
+		System.out.println("주석 ");
+		
+		// 한줄 주석은 slash 두개
+		System.out.println("한 줄 주석");
+		
+		/*
+		 * 여러줄 주석은 slash 별 ~~ 별 slash 까지
+		 * 
+		 * 
+		 */
+		System.out.println("여러 줄 주석");
+		
+	}
+}
