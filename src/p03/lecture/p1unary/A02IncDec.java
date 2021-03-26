7f55f1870b298118ddb2b64e014945a58110679e
sebaek <sebaek@gmail.com>
2021-03-26 15:42:46 +0900

    inc dec

---
+++ src/p03/lecture/p1unary/A02IncDec.java
@@ -0,0 +1,30 @@
+package p03.lecture.p1unary;
+
+public class A02IncDec {
+	public static void main(String[] args) {
+		// 증가 연산자
+		// ++ : 변수에 1더한 값을 다시 변수에 할당
+		
+		int x = 5;
+		x++;
+		System.out.println(x);
+		
+		// 감소 연산자
+		// -- : 변수에 1뺀 값을 다시 변수에 할당
+		x--;
+		System.out.println(x);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
