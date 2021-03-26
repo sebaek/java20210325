a3238219726c25a4a3e8f412bf1a09711dd621b6
sebaek <sebaek@gmail.com>
2021-03-26 11:11:50 +0900

    solution

---
+++ src/p02/textbook/exercise/Solution01.java
@@ -0,0 +1,76 @@
+package p02.textbook.exercise;
+
+public class Solution01 {
+	public static void main(String[] args) {
+		// 1번문제: 4
+		int v = 3;
+		
+		System.out.println(v);
+		
+		// 2번문제;
+		int modelName; 
+//		int class; 
+//		int 6hour;
+		int $value;
+		int _age;
+//		int int;
+		
+		// 3번문제;
+		
+		// 1byte        2byte         4byte          8byte
+		// 정수
+		// byte         short,char    int            long
+		// 실수
+		//                            float          double
+		// 논리
+		// boolean
+		
+		
+		// String 문자열 ""
+		
+		/////////////////////////////////
+		// 4번 문제
+		int age;
+		age = 10;
+		double price = 3.14;
+		
+		// 타입: int, double
+		// 변수명: age, price
+		// 리터럴: 10, 3.14
+		
+		/////////////
+		// 7번 문제;
+		
+		int var1 = 10;
+		long var2= 10000000000000L;
+//		char var3 = '';
+		double var4 = 10;
+		float var5 = 10;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
