267395590893d69592e9f83fad2a75961d8b07c5
sebaek <sebaek@gmail.com>
2021-03-26 15:12:39 +0900

    exercise

--- src/p02/textbook/exercise/Solution01.java
+++ src/p02/textbook/exercise/Solution01.java
@@ -38,6 +38,35 @@ public class Solution01 {
 		// 변수명: age, price
 		// 리터럴: 10, 3.14
 		
+		
+		///////////////
+		// 5번 문제
+		byte byteValue = 10;
+		char charValue = 'A';
+		
+		int intValue1 = byteValue;
+		int intValue2 = charValue;
+//		short shortValue3 = charValue;
+		double doubleValue4 = byteValue;
+		
+		
+		////////////////
+		// 6번 문제;
+		int intValue = 10;
+		char charValue6 = 'A';
+		double doubleValue = 5.7;
+		String strValue = "A";
+		
+		double var1 = (double) intValue;
+		byte var2 = (byte) intValue;
+		int var3 = (int) doubleValue;
+//		char var4 = (char) strValue;
+		
+		
+		
+		
+		
+		
 		/////////////
 		// 7번 문제;
 		
@@ -46,6 +75,20 @@ public class Solution01 {
 //		char var3 = '';
 		double var4 = 10;
 		float var5 = 10;
+		
+		
+		
+		/////////////
+		// 8번 문제;
+		byte byteValue8 = 10;
+		float floatValue8 = 2.5F;
+		double doubleValue8 = 2.5;
+		
+		byte result1 = byteValue8 + byteValue8;
+		int result2 = 5 + byteValue8;
+		float result3 = 5 + floatValue8;
+		double result4 = 5 + doubleValue8;
+		
 	}
 }
 
