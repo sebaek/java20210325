fc40941bd5ba0f27fae20004e7f29d0c4981dce0
sebaek <sebaek@gmail.com>
2021-03-29 14:18:06 +0900

    compare

--- src/p03/lecture/p2binary/p2compare/A01Compare.java
+++ src/p03/lecture/p2binary/p2compare/A01Compare.java
@@ -1,5 +1,28 @@
 package p03.lecture.p2binary.p2compare;
 
 public class A01Compare {
-
+	public static void main(String[] args) {
+		// == (같다), != (다르다)
+		// < (작다), <= (작거나 같다)
+		// > (크다), >= (크거나 같다)
+		// 비교연산자의 결과는 boolean 
+		
+		int a = 3;
+		int b = 8;
+		
+		System.out.println(a == b);
+		System.out.println(a != b);
+		System.out.println(a < b);
+		System.out.println(a <= b);
+		System.out.println(a > b);
+		System.out.println(a >= b);
+		
+		
+	}
 }
+
+
+
+
+
+
