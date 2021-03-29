ad50dd5cdf40cb28d52051aed15300686a224199
sebaek <sebaek@gmail.com>
2021-03-29 16:23:12 +0900

    warmup1

--- src/p99/codingbat/WarmUp1Solution.java
+++ src/p99/codingbat/WarmUp1Solution.java
@@ -1,6 +1,30 @@
 package p99.codingbat;
 
 public class WarmUp1Solution {
+	public boolean icyHot(int temp1, int temp2) {
+
+		if (temp1 < 0 && temp2 > 100) {
+			return true;
+		}
+
+		if (temp1 > 100 && temp2 < 0) {
+			return true;
+		}
+
+		return false;
+		/*
+		 * return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
+		 */
+	}
+
+	public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+		return aSmile == bSmile;
+	}
+
+	public boolean sleepIn(boolean weekday, boolean vacation) {
+		return !weekday || vacation;
+	}
+
 	public String backAround(String str) {
 		String res = "";
 		int len = str.length();
