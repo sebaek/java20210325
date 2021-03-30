905d1b36024596708bb6f57540a5663634309afc
sebaek <sebaek@gmail.com>
2021-03-30 15:05:48 +0900

    dice

--- src/p04/lecture/A04IfSample.java
+++ src/p04/lecture/A04IfSample.java
@@ -4,12 +4,22 @@ public class A04IfSample {
 	public static void main(String[] args) {
 		
 		int user = 3;
-		int dice = ; // 1~6 임의의 수 
+		int dice = (int) (Math.random() * 6) + 1; // 1~6 임의의 수 
 				
 		// 만약 user와 dice가 같으면
 		// "당첨되었습니다." 메세지 출력
 		// 그렇지 않으면 
 		// "다시 실행해보세요." 메세지 출력
-		
+
+		if (user == dice) {
+			System.out.println("당첨되었습니다.");
+		} else {
+			System.out.println("다시 실행해보세요.");
+		}
 	}
 }
+
+
+
+
+
---
+++ src/p04/lecture/A05IfSample.java
@@ -0,0 +1,5 @@
+package p04.lecture;
+
+public class A05IfSample {
+
+}
