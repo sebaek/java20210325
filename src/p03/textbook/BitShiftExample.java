9c6001c9fecbf0de3d143c744c24654b681c277e
sebaek <sebaek@gmail.com>
2021-03-30 10:20:42 +0900

    assign

--- src/p03/textbook/BitShiftExample.java
+++ src/p03/textbook/BitShiftExample.java
@@ -1,5 +1,9 @@
 package p03.textbook;
 
 public class BitShiftExample {
-
+	public static void main(String[] args) {
+		System.out.println("1 << 3 = " + (1 << 3));
+		System.out.println("-8 >> 3 = " + (-8 >> 3));
+		System.out.println("-8 >>> 3 = " + (-8 >>> 3));
+	}
 }
