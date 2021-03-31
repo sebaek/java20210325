8e316fc24c9ecf7af5ba5ffda430b9af03a8dd01
sebaek <sebaek@gmail.com>
2021-03-31 12:12:04 +0900

    starts

---
+++ src/p04/textbook/ForMultiplicationTableExample2.java
@@ -0,0 +1,53 @@
+package p04.textbook;
+
+public class ForMultiplicationTableExample2 {
+	public static void main(String[] args) {
+		/*
+		System.out.println("새로운 줄");
+		System.out.println("새로운 줄");
+		System.out.print("같은 줄");
+		System.out.print("같은 줄");
+		System.out.println("새로운 줄");
+		 */
+		/*
+		*
+		**
+		***
+		****
+		*****
+		 */
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		/*
+		 *****
+		 ****
+		 ***
+		 **
+		 *
+		 */
+		/*
+		 *****
+		  ****
+		   ***
+		    **
+		     *
+		 */
+		/*
+		     *
+		    **
+		   ***
+		  ****
+		 *****
+		 */
+
+	}
+}
+
+
+
+
+
