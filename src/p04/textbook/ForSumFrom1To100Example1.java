7bcf1b1c48d26bf261d8f4920aa8d8ba4341ffa8
sebaek <sebaek@gmail.com>
2021-03-31 11:12:38 +0900

    scope

--- src/p04/textbook/ForSumFrom1To100Example1.java
+++ src/p04/textbook/ForSumFrom1To100Example1.java
@@ -1,5 +1,13 @@
 package p04.textbook;
 
 public class ForSumFrom1To100Example1 {
+	public static void main(String[] args) {
+		int sum = 0;
 
+		for (int i = 1; i <= 100; i++) {
+			sum += i;
+		}
+
+		System.out.println("1~100 합 : " + sum);
+	}
 }
