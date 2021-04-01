db7fb7b2e0b2e65af37732df7b48375edf3721c6
sebaek <sebaek@gmail.com>
2021-04-01 15:30:18 +0900

    ref type

--- src/p05/lecture/A02ReferenceType.java
+++ src/p05/lecture/A02ReferenceType.java
@@ -8,5 +8,13 @@ public class A02ReferenceType {
 		String s;
 		s = new String("java");
 		
+		// String : 타입
+		// s : (참조)변수
+		// new String("java") : 객체(instance(개체), object(객체))
+		// s 가 가진 값: 참조값
+		
 	}
 }
+
+
+
