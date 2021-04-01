c0e6190628f6981bd103fbf5535461b5974cc0f5
sebaek <sebaek@gmail.com>
2021-04-01 14:13:07 +0900

    exercise

---
+++ src/p04/textbook/exercise/Exercise06.java
@@ -0,0 +1,12 @@
+package p04.textbook.exercise;
+
+public class Exercise06 {
+	public static void main(String[] args) {
+		String line = "";
+		
+		for (int i = 0; i < 5; i++) {
+			line += "*";
+			System.out.println(line);
+		}
+	}
+}
