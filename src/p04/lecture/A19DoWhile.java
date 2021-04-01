3bee6f92243d3679b15299bcc4672a32103466d2
sebaek <sebaek@gmail.com>
2021-04-01 10:41:00 +0900

    do while

---
+++ src/p04/lecture/A19DoWhile.java
@@ -0,0 +1,29 @@
+package p04.lecture;
+
+public class A19DoWhile {
+	public static void main(String[] args) {
+		boolean a = false;
+		while (a) {
+			System.out.println("while code block");
+		}
+		
+		// do while();  :  코드 블럭이 최초 한번은 실행됨
+		//              이후 조건이 true --> 코드 블럭
+		//                         false --> 다음 실행 흐름. 
+		
+		do {
+			System.out.println("do while code block");
+		} while (a);
+		
+		System.out.println("프로그램 종료...");
+	}
+}
+
+
+
+
+
+
+
+
+
