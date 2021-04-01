614ec0e497818f30fefd7d3f8867c626ac79e066
sebaek <sebaek@gmail.com>
2021-04-01 10:07:08 +0900

    while

--- src/p04/lecture/A17WhileSample.java
+++ src/p04/lecture/A17WhileSample.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class A17WhileSample {
 	public static void main(String[] args) {
 		int comNumber = (int) (Math.random() * 100) + 1;
-		System.out.println("comNumber:" + comNumber);
+//		System.out.println("comNumber:" + comNumber);
 		
 		Scanner scanner = new Scanner(System.in);
 		
@@ -13,12 +13,14 @@ public class A17WhileSample {
 			System.out.println("컴퓨터의 수를 맞춰보세요>");
 			int userNumber = scanner.nextInt();
 			
-			System.out.println("맞췄습니다");
-			break;
-			
-			System.out.println("더 큰 수를 입력해보세요.");
-			
-			System.out.println("더 작은 수를 입력해보세요.");
+			if (userNumber == comNumber){
+				System.out.println("맞췄습니다");
+				break;
+			} else if (userNumber < comNumber) {
+				System.out.println("더 큰 수를 입력해보세요.");
+			} else {
+				System.out.println("더 작은 수를 입력해보세요.");
+			}
 			
 		}
 
