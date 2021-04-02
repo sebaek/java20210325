c58e9ea0f42a98950f8969ec09a782c33b1991d3
sebaek <sebaek@gmail.com>
2021-04-02 15:30:08 +0900

    exercise 07

---
+++ src/p05/textbook/exercise/Exercise07.java
@@ -0,0 +1,15 @@
+package p05.textbook.exercise;
+
+public class Exercise07 {
+	public static void main(String[] args) {
+		int max = 0;
+		
+		int[] array = {1, 5, 3, 8, 2};
+		// 최대값
+		// 최소값
+		// 합계
+		// 평균
+		
+		System.out.println("max : " + max);
+	}
+}
