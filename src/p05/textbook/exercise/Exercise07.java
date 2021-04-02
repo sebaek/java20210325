6a201e6368a5f51588013bbeac809b137eec9fed
sebaek <sebaek@gmail.com>
2021-04-02 16:20:44 +0900

    min, avg, sum

--- src/p05/textbook/exercise/Exercise07.java
+++ src/p05/textbook/exercise/Exercise07.java
@@ -2,9 +2,10 @@ package p05.textbook.exercise;
 
 public class Exercise07 {
 	public static void main(String[] args) {
-		int max = 0;
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
 		
-		int[] array = {1, 5, 3, 8, 2};
+		int[] array = {-11, -15, -13, -18, -12};
 		// 최대값
 		for (int i = 0; i < array.length; i++) {
 			if (max < array[i]) {
@@ -12,10 +13,23 @@ public class Exercise07 {
 			}
 		}
 		// 최소값
+		for (int i = 0; i < array.length; i++) {
+			if (min > array[i]) {
+				min = array[i];
+			}
+		}
 		// 합계
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
 		// 평균
+		double avg = sum / (double) array.length;
 		
 		System.out.println("max : " + max);
+		System.out.println("min : " + min);
+		System.out.println("sum : " + sum);
+		System.out.println("avg : " + avg);
 	}
 }
 
