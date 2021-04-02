139bb5bce6e78462476d4a582564f38f60691886
sebaek <sebaek@gmail.com>
2021-04-02 10:27:27 +0900

    array

---
+++ src/p05/lecture/A10Array.java
@@ -0,0 +1,26 @@
+package p05.lecture;
+
+public class A10Array {
+	// 그림 : 06.배열.png
+	public static void main(String[] args) {
+		// array(배열) : 여러개의 값을 갖는 자료형
+		
+		int i;
+		i = 5;
+		
+		int[] arr; // 배열도 참조타입
+		arr = new int[3];
+		
+		arr[0] = 5;
+		arr[1] = 7;
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		
+	}
+}
+
+
+
+
