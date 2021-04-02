697ecdeba6b643d9bae4daf768f4d387d140a97f
sebaek <sebaek@gmail.com>
2021-04-02 14:34:33 +0900

    avg

---
+++ src/p05/lecture/A18ArraySample.java
@@ -0,0 +1,23 @@
+package p05.lecture;
+
+public class A18ArraySample {
+	public static void main(String[] args) {
+		int[] arr1 = new int[] {3, 4, 5, 6, 7};
+		int[] arr2 = new int[] {99, 88, 70};
+		
+		double avg1 = avg(arr1);
+		double avg2 = avg(arr2);
+		
+		System.out.println(avg1);
+		System.out.println(avg2);
+	}
+	
+	public static double avg(int[] arr) {
+		// 작성할 곳..
+	}
+}
+
+
+
+
+
