0a895ca3e9776521e60b2f727ebd90695219547f
sebaek <sebaek@gmail.com>
2021-04-02 10:43:24 +0900

    for

--- src/p05/lecture/A11ArrayField.java
+++ src/p05/lecture/A11ArrayField.java
@@ -7,16 +7,21 @@ public class A11ArrayField {
 		
 		System.out.println(arr1.length);
 		System.out.println(arr2.length);
-		
+		/*
 		arr1[0] = 10;
 		arr1[1] = 11;
 		arr1[2] = 12;
+		*/
+		// for
 		
+		/*
 		arr2[0] = 100;
 		arr2[1] = 200;
 		arr2[2] = 300;
 		arr2[3] = 400;
 		arr2[4] = 500;
+		*/
+		// for
 		
 		System.out.println("arr1 출력");
 		System.out.println(arr1[0]);
@@ -31,12 +36,14 @@ public class A11ArrayField {
 		System.out.println(arr2[4]);
 		
 		System.out.println("arr1 출력 (for)");
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < arr1.length; i++) {
 			System.out.println(arr1[i]);
 		}
 		
 		System.out.println("arr2 출력 (for)");
-		//
+		for (int i = 0; i < arr2.length; i++) {
+			System.out.println(arr2[i]);
+		}
 	}
 }
 
