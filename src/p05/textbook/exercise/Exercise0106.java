cf9fcce356187716fe98f2ec28803db8f5099a56
sebaek <sebaek@gmail.com>
2021-04-05 15:09:53 +0900

    exercise 01 /c/Users/user 06

---
+++ src/p05/textbook/exercise/Exercise0106.java
@@ -0,0 +1,36 @@
+package p05.textbook.exercise;
+
+public class Exercise0106 {
+	public static void main(String[] args) {
+		// 1번 문제:
+		// 답: 4번
+		
+		// 2번 문제:
+		// 답: 3번
+		
+		// 3번 문제:
+		// 답: 2번
+
+		// 4번 문제:
+		// 답: 2번
+		int[] array;
+		array = new int[] {1, 2, 3};
+		
+		// 5번 문제:
+		// 답: 3번 (boolean false)
+		
+		// 6번 문제:
+		// array.length --> 3
+		// array[2].length --> 5
+		int[][] array6 = {
+				{95, 86},
+				{83, 92, 96},
+				{78, 83, 93, 87, 88}
+		};
+	}
+}
+
+
+
+
+
