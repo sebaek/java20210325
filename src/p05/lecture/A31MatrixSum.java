998d7961606b188a9c33498136813ffb25dca3fb
sebaek <sebaek@gmail.com>
2021-04-06 10:17:09 +0900

    matrix sum

--- src/p05/lecture/A31MatrixSum.java
+++ src/p05/lecture/A31MatrixSum.java
@@ -18,17 +18,35 @@ public class A31MatrixSum {
 				{0, 1, 2},
 		};
 		
-		int[][] mat3 = sum(mat1, mat2);
+		int[][] mat3 = sum2(mat1, mat2);
 		
 		for (int[] row : mat3) {
 			System.out.println(Arrays.toString(row));
 		}
 	}
+	
+	private static int[][] sum2(int[][] a, int[][] b) {
+		int[][] c = new int[a.length][];
+		
+		for (int i = 0; i < c.length; i++) {
+			c[i] = new int[a[i].length];
+			for (int j = 0; j < c[i].length; j++) {
+				c[i][j] = a[i][j] + b[i][j];
+			}
+		}
+		
+		return c;
+	}
 
 	private static int[][] sum(int[][] a, int[][] b) {
-		int[][] c = new int[][]; // 수정해야함...
+		int[][] c = new int[a.length][a[0].length]; // 수정해야함...
 		
 		// 코드 작성....
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < c[i].length; j++) {
+				c[i][j] = a[i][j] + b[i][j];
+			}
+		}
 		
 		return c;
 	}
