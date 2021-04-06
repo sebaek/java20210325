8df4dd1a859d43cdca6773c73c2584bee434feab
sebaek <sebaek@gmail.com>
2021-04-06 14:17:35 +0900

    constructor

---
+++ src/p06/textbook/s060602/FieldInitValue.java
@@ -0,0 +1,17 @@
+package p06.textbook.s060602;
+
+public class FieldInitValue {
+	byte byteField;
+	short shortField;
+	int intField;
+	long longField;
+	
+	boolean booleanField;
+	char charField;
+	
+	float floatField;
+	double doubleField;
+	
+	int[] arrField;
+	String referenceField;
+}
---
+++ src/p06/textbook/s060703/KoreanExample.java
@@ -0,0 +1,5 @@
+package p06.textbook.s060703;
+
+public class KoreanExample {
+
+}
