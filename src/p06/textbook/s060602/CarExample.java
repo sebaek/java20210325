4767f90b331c219faf4ffc50b051d23e840a8150
sebaek <sebaek@gmail.com>
2021-04-06 12:36:30 +0900

    field

--- src/p06/textbook/s060602/CarExample.java
+++ src/p06/textbook/s060602/CarExample.java
@@ -3,6 +3,7 @@ package p06.textbook.s060602;
 public class CarExample {
 	public static void main(String[] args) {
 		Car myCar = new Car();
+		Car yourCar = new Car();
 		
 		System.out.println("제작회사: " + myCar.company);
 		System.out.println("모델명: " + myCar.model);
@@ -10,7 +11,22 @@ public class CarExample {
 		System.out.println("최고속도: " + myCar.maxSpeed);
 		System.out.println("현재속도: " + myCar.speed);
 		
+		System.out.println("===yourCar===");
+		System.out.println("제작회사: " + yourCar.company);
+		System.out.println("모델명: " + yourCar.model);
+		System.out.println("색깔: " + yourCar.color);
+		System.out.println("최고속도: " + yourCar.maxSpeed);
+		System.out.println("현재속도: " + yourCar.speed);
+		
+		System.out.println("===수정 후===");
 		myCar.speed = 60;
 		System.out.println("수정된 속도: " + myCar.speed);
+		System.out.println("수정된 속도: " + yourCar.speed);
 	}
 }
+
+
+
+
+
+
