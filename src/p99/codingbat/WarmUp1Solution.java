b29c59c71d2b540ef42d720e6026ba675c7e60e1
sebaek <sebaek@gmail.com>
2021-04-06 15:35:35 +0900

    not string

--- src/p99/codingbat/WarmUp1Solution.java
+++ src/p99/codingbat/WarmUp1Solution.java
@@ -1,6 +1,22 @@
 package p99.codingbat;
 
 public class WarmUp1Solution {
+	public String notString(String str) {
+		if (str.startsWith("not")) {
+			return str;
+		} else {
+			return "not " + str;
+		}
+
+		/*
+		 * if (str.length() < 3) { return "not " + str; }
+		 * 
+		 * String front = str.substring(0, 3);
+		 * 
+		 * if (front.equals("not")) { return str; } else { return "not " + str; }
+		 */
+	}
+
 	public boolean posNeg(int a, int b, boolean negative) {
 		if (negative) {
 			return (a < 0) && (b < 0);
