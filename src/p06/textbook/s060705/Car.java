9329a7bc738bd485594d243497f2a2fe670e6463
sebaek <sebaek@gmail.com>
2021-04-07 10:30:12 +0900

    constructor

--- src/p06/textbook/s060705/Car.java
+++ src/p06/textbook/s060705/Car.java
@@ -1,5 +1,32 @@
 package p06.textbook.s060705;
 
 public class Car {
+	String company = "현대자동차";
+	String model;
+	String color;
+	int maxSpeed;
 
+	Car() {
+
+	}
+
+	Car(String model) {
+		this(model, "은색", 250);
+	}
+
+	Car(String model, String color) {
+		this(model, color, 250);
+		System.out.println("string string int");
+	}
+
+	Car(String model, String color, int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
 }
+
+
+
+
+
