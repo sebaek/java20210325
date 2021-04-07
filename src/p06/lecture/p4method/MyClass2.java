e6ff77e6f08c94977719aef24aecf0abb64d8df3
sebaek <sebaek@gmail.com>
2021-04-07 11:41:26 +0900

    variables

---
+++ src/p06/lecture/p4method/MyClass2.java
@@ -0,0 +1,12 @@
+package p06.lecture.p4method;
+
+public class MyClass2 {
+	int a;
+	
+	void method1(int b) {
+		int c = 99;
+		System.out.println("a:" + a);
+		System.out.println("b:" + b);
+		System.out.println("c:" + c);
+	}
+}
