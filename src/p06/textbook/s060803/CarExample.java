de6a8a9d54e5077cc1530ef07262d0d9da175cd0
sebaek <sebaek@gmail.com>
2021-04-07 12:26:07 +0900

    method

--- src/p06/textbook/s060803/CarExample.java
+++ src/p06/textbook/s060803/CarExample.java
@@ -1,5 +1,11 @@
 package p06.textbook.s060803;
 
 public class CarExample {
-
+	public static void main(String[] args) {
+		Car myCar = new Car();
+		myCar.keyTurnOn();
+		myCar.run();
+		int speed = myCar.getSpeed();
+		System.out.println("현재 속도: " + speed + "km/h");
+	}
 }
