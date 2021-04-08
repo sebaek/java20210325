677f66e2444200a71d2af9c3d96573d44860e001
sebaek <sebaek@gmail.com>
2021-04-08 12:17:41 +0900

    static

---
+++ src/p06/lecture/p5static/MyClass2.java
@@ -0,0 +1,23 @@
+package p06.lecture.p5static;
+
+public class MyClass2 {
+	
+	// class field (= static field)
+	static int i;
+	
+	// instance field
+	int j;
+	
+	// class method (= static method)
+	static void method() {
+		System.out.println("method()");
+	}
+	
+	// instance method
+	void method1() {
+		System.out.println("method1()");
+	}
+}
+
+
+
