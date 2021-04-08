b9a3d53de194be63bb1a82db70a433e77e0bcc93
sebaek <sebaek@gmail.com>
2021-04-08 12:02:38 +0900

    ex16

--- src/p06/textbook/exercise/ex16/Printer.java
+++ src/p06/textbook/exercise/ex16/Printer.java
@@ -2,4 +2,23 @@ package p06.textbook.exercise.ex16;
 
 public class Printer {
 
+	public void println(int i) {
+		System.out.println(i);
+	}
+
+	public void println(boolean b) {
+		System.out.println(b);
+	}
+
+	public void println(double d) {
+		System.out.println(d);
+	}
+
+	public void println(String string) {
+		System.out.println(string);
+	}
+	
 }
+
+
+
