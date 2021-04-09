ccea3ca46873c5df93db0966042cf9b1056e2218
sebaek <sebaek@gmail.com>
2021-04-09 11:34:27 +0900

    class

--- src/p06/lecture/p8access_modifier/A04Constructor.java
+++ src/p06/lecture/p8access_modifier/A04Constructor.java
@@ -1,5 +1,24 @@
 package p06.lecture.p8access_modifier;
 
 public class A04Constructor {
+	// 클래스의 접근지정자는 public, (default)만 가능
+	
+	// 생성자를 만들지 않으면
+	// 자동으로 기본생성자(아규먼트 없는 생성자)가 추가되는데
+	// 접근지정자는 class의 접근지정가 붙는다.
+	
 
+	/*
+	private A04Constructor() {
+		
+	}
+	
+	public A04Constructor(int i) {
+		
+	}
+	
+	A04Constructor(int i, int j) {
+		
+	}
+	*/
 }
---
+++ src/p06/lecture/p8access_modifier/A05ClassAccessModifier.java
@@ -0,0 +1,13 @@
+package p06.lecture.p8access_modifier;
+
+public class A05ClassAccessModifier {
+
+}
+
+class OtherClass {
+	
+}
+
+class OtherClass2 {
+	
+}
