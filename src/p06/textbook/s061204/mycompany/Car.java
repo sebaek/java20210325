c56a6a7abe32b3e67d460c69cd7b30219b526b98
sebaek <sebaek@gmail.com>
2021-04-09 09:37:09 +0900

    import

---
+++ src/p06/textbook/s061204/mycompany/Car.java
@@ -0,0 +1,14 @@
+package p06.textbook.s061204.mycompany;
+
+import p06.textbook.s061204.hankook.*;
+import p06.textbook.s061204.hyndai.Engine;
+import p06.textbook.s061204.kumho.*;
+
+public class Car {
+	Engine engine = new Engine();
+	SnowTire tire1 = new SnowTire();
+	BigWidthTire tire2 = new BigWidthTire();
+	
+	p06.textbook.s061204.hankook.Tire tire3 = new p06.textbook.s061204.hankook.Tire();
+	p06.textbook.s061204.kumho.Tire tire4 = new p06.textbook.s061204.kumho.Tire();
+}
