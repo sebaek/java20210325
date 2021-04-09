6587346cd45f657dce29c65bea01dbd20c3aa277
sebaek <sebaek@gmail.com>
2021-04-09 12:39:08 +0900

    18

--- src/p06/textbook/exercise/ex18/ShopService.java
+++ src/p06/textbook/exercise/ex18/ShopService.java
@@ -1,5 +1,18 @@
 package p06.textbook.exercise.ex18;
 
 public class ShopService {
+	private static ShopService shopService;
+	
+	private ShopService() {
+		
+	}
+
+	public static ShopService getInstance() {
+		if (shopService == null) {
+			shopService = new ShopService();
+		}
+		
+		return shopService;
+	}
 
 }
