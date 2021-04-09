54251126b82893f9535f09eceeea6098f51dc61f
sebaek <sebaek@gmail.com>
2021-04-09 14:41:42 +0900

    ex19

--- src/p06/textbook/exercise/ex19/Account.java
+++ src/p06/textbook/exercise/ex19/Account.java
@@ -1,5 +1,17 @@
 package p06.textbook.exercise.ex19;
 
 public class Account {
-
+	private int balance;
+	private static final int MIN_BALANCE = 0;
+	private static final int MAX_BALANCE = 1_000_000;
+	
+	public void setBalance(int balance) {
+		if (balance >= MIN_BALANCE && balance <= MAX_BALANCE) {
+			this.balance = balance;
+		}
+	}
+	
+	public int getBalance() {
+		return balance;
+	}
 }
