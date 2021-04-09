884111a3f3f8171682e4ab6ccf23237a534bf1d7
sebaek <sebaek@gmail.com>
2021-04-09 11:22:57 +0900

    ex 01~12

---
+++ src/p06/textbook/exercise/ex0112/Exercise0112.java
@@ -0,0 +1,83 @@
+package p06.textbook.exercise.ex0112;
+
+public class Exercise0112 {
+	// 1번문제:
+	// 답 3번
+	
+	// 2번문제:
+	// 답 4번
+	
+	// 3번문제:
+	// 답 4번
+	
+	// 4번문제:
+	// 답 3번
+	
+	// 5번문제:
+	// 답 1번
+	
+	
+	// 6번문제:
+	// 답 4번 : method overloading
+	
+	// 7번문제:
+	// 답 2번
+	
+	// 8번문제:
+	// 답 2번
+
+	// 9번문제:
+	// 답 2번
+	
+	// 10번문제:
+	// 답 4번
+	
+	// 11번문제:
+	// 답 3번
+	
+	// 12번문제
+	private String name; // 필드
+	public Exercise0112(String name) {
+		// 생성자
+	}
+	public void setName(String name) {
+		// 메소드
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
