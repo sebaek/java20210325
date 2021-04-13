da3814d5ca81e88b80f7935922b9fc599ee77956
sebaek <sebaek@gmail.com>
2021-04-13 12:06:47 +0900

    casting

--- src/p07/lecture/p6polymorphism/ex2/Tiger.java
+++ src/p07/lecture/p6polymorphism/ex2/Tiger.java
@@ -6,4 +6,8 @@ public class Tiger extends KindaCat {
 	public void cry() {
 		System.out.println("어흥!!!!!!!!!!!!!");
 	}
+	
+	public void hunt() {
+		System.out.println("호랑이가 사냥합니다!");
+	}
 }
--- src/p07/textbook/s070704/Driver.java
+++ src/p07/textbook/s070704/Driver.java
@@ -3,7 +3,7 @@ package p07.textbook.s070704;
 public class Driver {
 
 	public void drive(Vehicle vehicle) {
-		vehicle.run();
+		vehicle.run(); 
 	}
 
 }
