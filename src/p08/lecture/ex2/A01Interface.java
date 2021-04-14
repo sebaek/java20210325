07e878705dea40e5ff0cbfea49d670b2d94394d1
sebaek <sebaek@gmail.com>
2021-04-14 10:14:48 +0900

    interface

--- src/p08/lecture/ex2/A01Interface.java
+++ src/p08/lecture/ex2/A01Interface.java
@@ -5,6 +5,20 @@ public class A01Interface {
 		System.out.println(MyInterface.MY_FIELD1);
 		System.out.println(MyInterface.MY_FIELD2);
 		
+		MyInterface.staticMethod1();
+		MyInterface.staticMethod2();
 		
+		MyClass c1 = new MyClass();
+		c1.method1();
+		c1.method2();
+		
+		c1.defaultMethod1();
+		c1.defaultMethod2();
 	}
 }
+
+
+
+
+
+
--- src/p08/lecture/ex2/MyInterface.java
+++ src/p08/lecture/ex2/MyInterface.java
@@ -4,11 +4,39 @@ public interface MyInterface {
 	// public static final 필드
 	public static final int MY_FIELD1 = 1;
 	int MY_FIELD2 = 2;
-	
-	// public abstract 메소드 (인스턴스 메소드)
+	String MY_FIELD3 = "hello";
 	
 	// public static 메소드 (클래스 메소드)
+	public static void staticMethod1() {
+		System.out.println("static method1");
+	}
+	
+	static void staticMethod2() {
+		System.out.println("static method2");
+	}
+	
+	// public abstract 메소드 (인스턴스 메소드)
+	public abstract void method1();
+	void method2();
 	
 	// public default 메소드 (인스턴스 메소드)
+	public default void defaultMethod1() {
+		System.out.println("default method1");
+	}
 	
+	default void defaultMethod2() {
+		System.out.println("default method2");
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
