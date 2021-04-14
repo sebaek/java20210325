4931c2a54853470fd45e458e0339fe8e9315ed33
sebaek <sebaek@gmail.com>
2021-04-14 12:11:22 +0900

    static

--- src/p08/textbook/s080403/RemoteControlExample.java
+++ src/p08/textbook/s080403/RemoteControlExample.java
@@ -5,5 +5,7 @@ import p08.textbook.s080205.RemoteControl;
 public class RemoteControlExample {
 	public static void main(String[] args) {
 		RemoteControl.changeBattery();
+		System.out.println(RemoteControl.MAX_VOLUME);
+		System.out.println(RemoteControl.MIN_VOLUME);
 	}
 }
