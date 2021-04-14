4cc67125c83ce76b7694871be71e608d91d5e3a5
sebaek <sebaek@gmail.com>
2021-04-14 11:08:22 +0900

    interface

---
+++ src/p08/lecture/ex3/A01Interface.java
@@ -0,0 +1,14 @@
+package p08.lecture.ex3;
+
+public class A01Interface {
+	public static void main(String[] args) {
+		Malamute d1 = new Malamute();
+		Pet p1 = d1;
+		p1.sit();
+//		p1.pull(); // x
+		
+		SledDog s1 = d1;
+//		s1.sit(); // x
+		s1.pull();
+	}
+}
--- src/p08/textbook/s080301/Audio.java
+++ src/p08/textbook/s080301/Audio.java
@@ -1,5 +1,30 @@
 package p08.textbook.s080301;
 
-public class Audio {
+import p08.textbook.s080205.RemoteControl;
 
+public class Audio implements RemoteControl {
+	private int volume;
+
+	@Override
+	public void turnOn() {
+		System.out.println("Audio를 켭니다.");
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("Audio를 끕니다.");
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		if (volume > RemoteControl.MAX_VOLUME) {
+			this.volume = RemoteControl.MAX_VOLUME;
+		} else if (volume < RemoteControl.MIN_VOLUME) {
+			this.volume = RemoteControl.MIN_VOLUME;
+		} else {
+			this.volume = volume;
+		}
+		System.out.println("현재 Audio 볼륨: " + this.volume);
+
+	}
 }
