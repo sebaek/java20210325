5b1e115ecb6bfac8aa07c8f4731e30111af3dbe9
sebaek <sebaek@gmail.com>
2021-04-14 15:10:29 +0900

    polymorphism

--- src/p08/textbook/s080503/Car.java
+++ src/p08/textbook/s080503/Car.java
@@ -1,5 +1,22 @@
 package p08.textbook.s080503;
 
-public class Car {
+import p08.textbook.s080502.HankookTire;
+import p08.textbook.s080502.Tire;
 
+public class Car {
+	Tire[] tires = {
+			new HankookTire(),
+			new HankookTire(),
+			new HankookTire(),
+			new HankookTire()
+	};
+	
+	void run() {
+		for (Tire tire : tires) {
+			tire.roll();
+		}
+	}
 }
+
+
+
