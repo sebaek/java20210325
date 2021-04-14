65907857ebf2cfec999d468ce03a660b990bba48
sebaek <sebaek@gmail.com>
2021-04-14 12:34:17 +0900

    hasteen

--- src/p99/codingbat/WarmUp1Solution.java
+++ src/p99/codingbat/WarmUp1Solution.java
@@ -1,6 +1,14 @@
 package p99.codingbat;
 
 public class WarmUp1Solution {
+	public boolean hasTeen(int a, int b, int c) {
+		boolean aTeen = (a >= 13) && (a <= 19);
+		boolean bTeen = (b >= 13) && (b <= 19);
+		boolean cTeen = (c >= 13) && (c <= 19);
+
+		return aTeen || bTeen || cTeen;
+	}
+
 	public boolean startHi(String str) {
 		if (str.length() >= 2) {
 			char first = str.charAt(0);
