264b2610a7932a2b90d4dbb060b2552610d7bf11
sebaek <sebaek@gmail.com>
2021-04-15 14:33:24 +0900

    ex5

---
+++ src/p08/textbook/exercise/ex05/Action.java
@@ -0,0 +1,5 @@
+package p08.textbook.exercise.ex05;
+
+public interface Action {
+	void work();
+}
---
+++ src/p08/textbook/exercise/ex05/ActionExample.java
@@ -0,0 +1,14 @@
+package p08.textbook.exercise.ex05;
+
+public class ActionExample {
+	public static void main(String[] args) {
+		Action action = new Action() {
+			@Override
+			public void work() {
+				System.out.println("복사를 합니다.");
+			}
+		};
+		
+		action.work();
+	}
+}
