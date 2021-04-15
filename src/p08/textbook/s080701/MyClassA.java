a344b31f4202197ae4ab3480db293c139b7c50da
sebaek <sebaek@gmail.com>
2021-04-15 15:15:45 +0900

    default method

---
+++ src/p08/textbook/s080701/MyClassA.java
@@ -0,0 +1,8 @@
+package p08.textbook.s080701;
+
+public class MyClassA implements MyInterface {
+	@Override
+	public void method1() {
+		System.out.println("myclassa - method1");
+	}
+}
---
+++ src/p08/textbook/s080701/MyClassB.java
@@ -0,0 +1,13 @@
+package p08.textbook.s080701;
+
+public class MyClassB implements MyInterface {
+	@Override
+	public void method1() {
+		System.out.println("myclassb-method1()");
+	}
+	
+	@Override
+	public void method2() {
+		System.out.println("myclassb-method2()");
+	}
+}
