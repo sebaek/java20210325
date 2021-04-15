c8bf0820f1e59dfbc167cd0b8d5f73d5bf6edb86
sebaek <sebaek@gmail.com>
2021-04-15 13:49:34 +0900

    anonymous class

---
+++ src/p08/textbook/s080302/RemoteControlExample.java
@@ -0,0 +1,23 @@
+package p08.textbook.s080302;
+
+import p08.textbook.s080205.RemoteControl;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		RemoteControl rc = new RemoteControl() {
+
+			@Override
+			public void turnOn() {
+			}
+
+			@Override
+			public void turnOff() {
+			}
+
+			@Override
+			public void setVolume(int volume) {
+			}
+			
+		};
+	}
+}
