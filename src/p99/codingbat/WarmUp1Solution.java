f9c5030906e9b7eaabf1977cd7df2551775db034
sebaek <sebaek@gmail.com>
2021-04-15 15:41:39 +0900

    loneteen

--- src/p99/codingbat/WarmUp1Solution.java
+++ src/p99/codingbat/WarmUp1Solution.java
@@ -1,6 +1,13 @@
 package p99.codingbat;
 
 public class WarmUp1Solution {
+	public boolean loneTeen(int a, int b) {
+		boolean aTeen = (13 <= a) && (a <= 19);
+		boolean bTeen = (13 <= b) && (b <= 19);
+
+		return aTeen ^ bTeen;
+	}
+
 	public boolean hasTeen(int a, int b, int c) {
 		boolean aTeen = (a >= 13) && (a <= 19);
 		boolean bTeen = (b >= 13) && (b <= 19);
