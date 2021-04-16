a72f57f1bbbb7f572ee9c1087e07b5ffced57779
sebaek <sebaek@gmail.com>
2021-04-16 14:24:27 +0900

    local class

---
+++ src/p09/textbook/exercise/ex06/Chatting.java
@@ -0,0 +1,30 @@
+package p09.textbook.exercise.ex06;
+
+public class Chatting {
+	void start(String chatId) {
+		String nickName = null;
+		nickName = chatId;
+
+		Chat chat = new Chat() {
+
+			@Override
+			public void start() {
+				while (true) {
+					String inputData = "안녕하세요";
+					String message = "[" + nickName + "]" + inputData;
+					sendMessage(message);
+				}
+			}
+		};
+
+		chat.start();
+	}
+
+	class Chat {
+		void start() {
+		}
+
+		void sendMessage(String message) {
+		}
+	}
+}
