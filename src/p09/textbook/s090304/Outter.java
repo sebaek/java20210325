89d9af25ca2c353f9847a961094c4e7da27b8478
sebaek <sebaek@gmail.com>
2021-04-16 11:38:13 +0900

    this

---
+++ src/p09/textbook/s090304/Outter.java
@@ -0,0 +1,25 @@
+package p09.textbook.s090304;
+
+public class Outter {
+	String field = "Outter-field";
+
+	void method() {
+		System.out.println("Outter-method");
+	}
+
+	class Nested {
+		String field = "Nested-field";
+
+		void method() {
+			System.out.println("Nested-method");
+		}
+		
+		void print() {
+			System.out.println(this.field);
+			this.method();
+			
+			System.out.println(Outter.this.field);
+			Outter.this.method();
+		}
+	}
+}
