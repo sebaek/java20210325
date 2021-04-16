be207435d74fdff89a98a3a7d7fbe58bbc566637
sebaek <sebaek@gmail.com>
2021-04-16 11:26:34 +0900

    local class access

--- src/p09/textbook/s090303/Outter.java
+++ src/p09/textbook/s090303/Outter.java
@@ -2,4 +2,40 @@ package p09.textbook.s090303;
 
 public class Outter {
 
+	public void method1(final int arg) {
+		final int localVarialbe = 1;
+//		arg = 100;
+//		localVarialbe = 100;
+		
+		class Inner {
+			public void method() {
+				int result = arg + localVarialbe;
+			}
+		}
+	}
+	
+	
+	public void method2(int arg) {
+		int localVariale = 1;
+//		arg = 100;
+//		localVariale = 100;
+		
+		class Inner {
+			public void method() {
+				int result = arg + localVariale;
+			}
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
