99d20188d4af7426a21adfc41ff3f38777e346ba
sebaek <sebaek@gmail.com>
2021-04-16 15:06:18 +0900

    anonymous

--- src/p09/textbook/s090502/AnonymousExample.java
+++ src/p09/textbook/s090502/AnonymousExample.java
@@ -1,5 +1,24 @@
 package p09.textbook.s090502;
 
 public class AnonymousExample {
-
+	public static void main(String[] args) {
+		Anonymous anony = new Anonymous();
+		
+		anony.field.turnOn();
+		
+		anony.method1();
+		
+		anony.method2(new RemoteControl() {
+			
+			@Override
+			public void turnOn() {
+				System.out.println("SmartTV를 켭니다.");
+			}
+			
+			@Override
+			public void turnOff() {
+				System.out.println("SmartTV를 끕니다.");
+			}
+		});
+	}
 }
