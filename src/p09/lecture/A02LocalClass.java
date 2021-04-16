b7656b7048a44877ae652f624a7e1a22bc0fbc36
sebaek <sebaek@gmail.com>
2021-04-16 09:37:08 +0900

    local class

---
+++ src/p09/lecture/A02LocalClass.java
@@ -0,0 +1,30 @@
+package p09.lecture;
+
+public class A02LocalClass {
+	// non-static inner class
+	class InnerClass {
+		
+	}
+	
+	// static nested class
+	static class NestedClass {
+		
+	}
+	
+	void method1() {
+		// local class
+		class LocalClass {
+			// 필드
+			// 생성자
+			// 메소드
+		}
+		LocalClass o1 = new LocalClass();
+		
+	}
+}
+
+
+
+
+
+
---
+++ src/p09/textbook/exercise/ex04/Car.java
@@ -0,0 +1,6 @@
+package p09.textbook.exercise.ex04;
+
+public class Car {
+	class Tire {}
+	static class Engine{}
+}
