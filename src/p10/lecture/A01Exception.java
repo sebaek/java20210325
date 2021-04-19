b6f8afafb7fdeeada12452d7a8ec33e9d7e928c3
sebaek <sebaek@gmail.com>
2021-04-19 09:26:01 +0900

    exception

---
+++ src/p10/lecture/A01Exception.java
@@ -0,0 +1,20 @@
+package p10.lecture;
+
+public class A01Exception {
+	// 예외상황(Exception)
+	// 1. ERROR
+	//    예) 정전, 코드 절단, 번개
+	
+	// 2. Exception
+	//    2.1. Checked Exception
+	//         컴파일러가 검사(check)함
+	
+	//    2.2. UnChecked Exception(Runtime Exception)
+	//         컴파일러가 검사안함(unchecked)
+	//         실행시(Runtime) 발생
+}
+
+
+
+
+
---
+++ src/p10/lecture/A02NullPointerException.java
@@ -0,0 +1,24 @@
+package p10.lecture;
+
+public class A02NullPointerException {
+	public static void main(String[] args) {
+		System.out.println("프로그램 실행중..");
+		
+		String str = "java";
+		showLength(str);
+		
+		str = null; // null : 참조하는 객체가 없다
+		showLength(str);
+		
+		System.out.println("프로그램 종료..");
+	}
+	
+	private static void showLength(String s) {
+		System.out.println("길이는" + s.length());
+	}
+}
+
+
+
+
+
