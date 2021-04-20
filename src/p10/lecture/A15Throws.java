d79a3891b2b8a00b3764943a2ce240d9ca6c1709
sebaek <sebaek@gmail.com>
2021-04-20 09:37:47 +0900

    throws

---
+++ src/p10/lecture/A15Throws.java
@@ -0,0 +1,27 @@
+package p10.lecture;
+
+public class A15Throws {
+	public static void main(String[] args) throws ClassNotFoundException {
+		System.out.println("프로그램 실행");
+		method1();
+		System.out.println("프로그램 종료");
+	}
+	
+	public static void method1() throws ClassNotFoundException {
+		method2();
+	}
+	
+	public static void method2() throws ClassNotFoundException {
+		method3();
+	}
+	
+	public static void method3() throws ClassNotFoundException {
+		Class.forName("java.lang.Number2");
+	}
+}
+
+
+
+
+
+
