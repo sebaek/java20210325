2c11d34218f55f07ee5da33d7bf5f2f776bb975a
sebaek <sebaek@gmail.com>
2021-04-20 11:39:50 +0900

    custom exception

--- src/p10/textbook/s100701/BalanceInsufficientException.java
+++ src/p10/textbook/s100701/BalanceInsufficientException.java
@@ -1,5 +1,12 @@
 package p10.textbook.s100701;
 
-public class BalanceInsufficientException {
+public class BalanceInsufficientException extends Exception {
+	public BalanceInsufficientException() {
+		
+	}
+	
+	public BalanceInsufficientException(String message) {
+		super(message);
+	}
 
 }
