7db5ee701f434d57852bb5fad87d93ad0b338335
sebaek <sebaek@gmail.com>
2021-04-20 11:08:16 +0900

    throw

---
+++ src/p10/lecture/A18ThrowKeyword.java
@@ -0,0 +1,35 @@
+package p10.lecture;
+
+public class A18ThrowKeyword {
+	public static void main(String[] args) {
+		method1();
+		
+		try {
+			method2();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void method1() {
+		int i = 0;
+		
+		if (i == 0) {
+			throw new RuntimeException();
+		}
+	}
+	
+	private static void method2() throws Exception {
+		int i = 0;
+		
+		if (i == 0) {
+			throw new Exception();
+		}
+	}
+}
+
+
+
+
+
