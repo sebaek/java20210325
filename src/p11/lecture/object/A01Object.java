f7638b4d51286899d6a42b9948ed9a30ec3aab02
sebaek <sebaek@gmail.com>
2021-04-20 14:13:47 +0900

    object

---
+++ src/p11/lecture/object/A01Object.java
@@ -0,0 +1,20 @@
+package p11.lecture.object;
+
+public class A01Object {
+	// Object 클래스는 
+	// 모든 클래스의 상위 클래스
+	
+	public static void main(String[] args) {
+		MyClass o1 = new MyClass();
+		int i = o1.hashCode();
+		String s = o1.toString();
+	}
+}
+
+class MyClass {
+	
+}
+
+
+
+
---
+++ src/p11/lecture/object/A02Equals.java
@@ -0,0 +1,5 @@
+package p11.lecture.object;
+
+public class A02Equals {
+
+}
