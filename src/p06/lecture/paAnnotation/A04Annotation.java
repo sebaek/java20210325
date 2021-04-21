0405204235d1e263728f5848a8f4c69e9acf55d6
sebaek <sebaek@gmail.com>
2021-04-21 14:38:39 +0900

    element value

--- src/p06/lecture/paAnnotation/A04Annotation.java
+++ src/p06/lecture/paAnnotation/A04Annotation.java
@@ -1,9 +1,19 @@
 package p06.lecture.paAnnotation;
 
+@MyAnnotation3
 public class A04Annotation {
+	@MyAnnotation3
+	int field;
+	
+	@MyAnnotation3
+	A04Annotation() {
+		
+	}
+	
 	@MyAnnotation3(999)
 	public void method1() {
-		
+		@MyAnnotation3
+		int val;
 	}
 	
 	@MyAnnotation3(value = 999)
---
+++ src/p06/lecture/paAnnotation/A05Annotaion.java
@@ -0,0 +1,5 @@
+package p06.lecture.paAnnotation;
+
+public class A05Annotaion {
+
+}
