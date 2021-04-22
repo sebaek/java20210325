989e3d0a26b526cdea56d885f6c740beba6ed328
sebaek <sebaek@gmail.com>
2021-04-22 12:14:07 +0900

    regex

---
+++ src/p11/lecture/regex/A01RegularExpression.java
@@ -0,0 +1,58 @@
+package p11.lecture.regex;
+
+import java.util.regex.Pattern;
+
+public class A01RegularExpression {
+	// 정규표현식
+	// 문자열의 패턴을 표현하는 문자열
+	// meta-character, quantifier
+	
+	public static void main(String[] args) {
+		String regex1 = "a";
+		String str1 = "a";
+		
+		boolean p1 = Pattern.matches(regex1, str1);
+		System.out.println(p1);
+		
+		regex1 = "java";
+		str1 = "java";
+		
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "9";
+		str1 = "0";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d";
+		str1 = "8";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d";
+		str1 = "89";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d\\d";
+		str1 = "67";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d{3}";
+		str1 = "943";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "\\d{3,}";
+		str1 = "987139785";
+		System.out.println(Pattern.matches(regex1, str1));
+		
+		regex1 = "010-\\d{4}-\\d{4}";
+		str1 = "010-9876-3333";
+		System.out.println(Pattern.matches(regex1, str1));
+	}
+}
+
+
+
+
+
+
+
+
