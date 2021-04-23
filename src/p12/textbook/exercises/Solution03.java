881606976eea408ab68a6e5900e9b985d4a03e60
sebaek <sebaek@gmail.com>
2021-04-23 15:26:11 +0900

    solution

---
+++ src/p12/textbook/exercises/Solution03.java
@@ -0,0 +1,6 @@
+package p12.textbook.exercises;
+
+public class Solution03 {
+	//3번문제
+	//답 : 4 
+}
---
+++ src/p12/textbook/exercises/Solution04.java
@@ -0,0 +1,7 @@
+package p12.textbook.exercises;
+
+public class Solution04 {
+	// 4번문제
+	// 답: 2번
+	
+}
