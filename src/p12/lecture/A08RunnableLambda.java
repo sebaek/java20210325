e3a9571cdfd023da48f6f70e8266e41ca87b953e
sebaek <sebaek@gmail.com>
2021-04-23 11:18:30 +0900

    thread name

---
+++ src/p12/lecture/A08RunnableLambda.java
@@ -0,0 +1,22 @@
+package p12.lecture;
+
+public class A08RunnableLambda {
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("thread1 실행");
+			}
+		});
+		t1.start();
+
+		// Lambda 식
+		Thread t2 = new Thread(() -> System.out.println("thread2 실행"));
+		t2.start();
+	}
+}
+
+
+
+
+
--- src/p12/textbook/s120201/BeepPrintExample3.java
+++ src/p12/textbook/s120201/BeepPrintExample3.java
@@ -1,12 +1,25 @@
 package p12.textbook.s120201;
 
+import java.awt.Toolkit;
+
 public class BeepPrintExample3 {
 	public static void main(String[] args) {
 		Thread thread = new Thread(new Runnable() {
-			// 작성....
+			@Override
+			public void run() {
+				Toolkit toolkit = Toolkit.getDefaultToolkit();
+				for (int i = 0; i < 5; i++) {
+					toolkit.beep();
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 		});
 		thread.start();
-		
+
 		for (int i = 0; i < 5; i++) {
 			System.out.println("띵");
 			try {
