70203e83b78af2797014548173900375697a8408
sebaek <sebaek@gmail.com>
2021-04-23 15:37:19 +0900

    atomic

--- src/p12/lecture/A16AtomicVariable.java
+++ src/p12/lecture/A16AtomicVariable.java
@@ -1,13 +1,17 @@
 package p12.lecture;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class A16AtomicVariable {
 	static int sv;
+	static AtomicInteger ai = new AtomicInteger();
 	
 	public static void main(String[] args) {
 		Thread t = new Thread(new Runnable() {
 			public void run() {
 				for (int i = 0; i < 1_000_000; i++) {
 					sv++;
+					ai.getAndIncrement();
 				}
 			}
 		});
@@ -15,15 +19,16 @@ public class A16AtomicVariable {
 		
 		for (int i = 0; i < 1_000_000; i++) {
 			sv++;
+			ai.getAndIncrement();
 		}
 		
 		try {
 			t.join();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		System.out.println(sv);
+		System.out.println(ai.get());
 	}
 }
 
