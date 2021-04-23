a0db1f683ee0835307d6aa393f4a8dcece6530de
sebaek <sebaek@gmail.com>
2021-04-23 14:10:13 +0900

    synchronized method

--- src/p12/lecture/A14SynchronizedMethod.java
+++ src/p12/lecture/A14SynchronizedMethod.java
@@ -1,5 +1,43 @@
 package p12.lecture;
 
 public class A14SynchronizedMethod {
-
+	
 }
+
+class Thread14 extends Thread {
+	public Box box;
+	
+	@Override
+	public void run() {
+		box.update(100);
+	}
+}
+
+class Thread15 extends Thread {
+	public Box box;
+	
+	@Override
+	public void run() {
+		box.update(50);
+	}
+}
+
+
+class Box {
+	public int i;
+	
+	public void update(int i) {
+		this.i = i;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
