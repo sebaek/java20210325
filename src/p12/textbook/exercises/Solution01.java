1f717e30d56f6d88e14fc42c44d524d28f1c8601
sebaek <sebaek@gmail.com>
2021-04-23 15:22:43 +0900

    solution

---
+++ src/p12/textbook/exercises/Solution01.java
@@ -0,0 +1,7 @@
+package p12.textbook.exercises;
+
+public class Solution01 {
+	// 1번문제
+	// 답 : 4번
+	
+}
---
+++ src/p12/textbook/exercises/ex02/MusicRunnable.java
@@ -0,0 +1,19 @@
+package p12.textbook.exercises.ex02;
+
+public class MusicRunnable implements Runnable {
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 3; i++) {
+			System.out.println("음악을 재생합니다.");
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
