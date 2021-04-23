888352a6bcf81ce7e86f00b6202cb69c9f3d8218
sebaek <sebaek@gmail.com>
2021-04-23 14:14:43 +0900

    synchronized method

--- src/p12/lecture/A14SynchronizedMethod.java
+++ src/p12/lecture/A14SynchronizedMethod.java
@@ -38,6 +38,14 @@ class Box {
 	
 	public void update(int i) {
 		this.i = i;
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		System.out.println(i);
 	}
 }
