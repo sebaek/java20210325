bc7d0d116a6ccedfe6214140d1317bfe4e742f58
sebaek <sebaek@gmail.com>
2021-04-23 10:20:44 +0900

    runnable beep

---
+++ src/p12/lecture/A06Runnable.java
@@ -0,0 +1,34 @@
+package p12.lecture;
+
+import java.awt.Toolkit;
+
+public class A06Runnable {
+	public static void main(String[] args) {
+		Thread t6 = new Thread(new Thread6());
+		t6.start();
+	}
+}
+
+class Thread6 implements Runnable {
+	@Override
+	public void run() {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		
+		for (int i = 0; i < 5; i++) {
+			toolkit.beep();
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+
+
+
+
+
