8c2147c20e79ac0c23258629258fd1f26ef7553a
sebaek <sebaek@gmail.com>
2021-04-23 12:15:38 +0900

    shared

--- src/p12/lecture/A12Concurrency.java
+++ src/p12/lecture/A12Concurrency.java
@@ -2,6 +2,31 @@ package p12.lecture;
 
 public class A12Concurrency {
 
+	static long shareValue = 0;
+	
+	public static void main(String[] args) {
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 100_0000; i++) {
+					shareValue++;
+				}
+			}
+		});
+		t.start();
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(shareValue);
+		
+	}
 }
+
+
+
+
 
 
