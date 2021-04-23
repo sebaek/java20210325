7abad96151034a4667858d38f79ca5c2eacd0546
sebaek <sebaek@gmail.com>
2021-04-23 09:42:00 +0900

    thread

--- src/p12/lecture/A03ThreadSleep.java
+++ src/p12/lecture/A03ThreadSleep.java
@@ -6,7 +6,7 @@ public class A03ThreadSleep {
 		System.out.println("실행1");
 		
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(5000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
