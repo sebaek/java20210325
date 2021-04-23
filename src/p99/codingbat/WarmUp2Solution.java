c2de64a96c9454b655621878d1f645fa4f255331
sebaek <sebaek@gmail.com>
2021-04-23 16:06:59 +0900

    warmup2

---
+++ src/p99/codingbat/WarmUp2Solution.java
@@ -0,0 +1,13 @@
+package p99.codingbat;
+
+public class WarmUp2Solution {
+	public String stringTimes(String str, int n) {
+		String res = "";
+
+		for (int i = 0; i < n; i++) {
+			res += str;
+		}
+
+		return res;
+	}
+}
