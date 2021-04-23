db483898bbf975cd40043b9a965af0c242e81b19
sebaek <sebaek@gmail.com>
2021-04-23 16:16:16 +0900

    warmup2

--- src/p99/codingbat/WarmUp2Solution.java
+++ src/p99/codingbat/WarmUp2Solution.java
@@ -1,6 +1,22 @@
 package p99.codingbat;
 
 public class WarmUp2Solution {
+	public String frontTimes(String str, int n) {
+		String res = "";
+
+		String front = str;
+
+		if (str.length() >= 3) {
+			front = str.substring(0, 3);
+		}
+
+		for (int i = 0; i < n; i++) {
+			res += front;
+		}
+
+		return res;
+	}
+
 	public String stringTimes(String str, int n) {
 		String res = "";
 
