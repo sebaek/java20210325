af5fad31d6119b98a4b229ef6405c889ba3d281e
sebaek <sebaek@gmail.com>
2021-04-26 11:13:40 +0900

    generic method

---
+++ src/p13/textbook/exercises/ex03/Container.java
@@ -0,0 +1,20 @@
+package p13.textbook.exercises.ex03;
+
+public class Container<K, V> {
+	private K key;
+	private V value;
+	
+	public void set(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+	
+	public K getKey() {
+		return key;
+	}
+	
+	public V getValue() {
+		return value;
+	}
+	
+}
---
+++ src/p13/textbook/s130401/Util.java
@@ -0,0 +1,5 @@
+package p13.textbook.s130401;
+
+public class Util {
+
+}
