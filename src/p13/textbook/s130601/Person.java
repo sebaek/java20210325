1ee7446850a13685ed25d9b4850857673c656210
sebaek <sebaek@gmail.com>
2021-04-26 15:28:48 +0900

    wildcard

---
+++ src/p13/textbook/s130601/Person.java
@@ -0,0 +1,14 @@
+package p13.textbook.s130601;
+
+public class Person {
+	private String name;
+
+	public Person(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+}
---
+++ src/p13/textbook/s130601/Student.java
@@ -0,0 +1,8 @@
+package p13.textbook.s130601;
+
+public class Student extends Person {
+
+	public Student(String name) {
+		super(name);
+	}
+}
