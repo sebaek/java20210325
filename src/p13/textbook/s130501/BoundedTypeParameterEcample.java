85a13952c08a4fd0bd57fb7c86aefe282060612c
sebaek <sebaek@gmail.com>
2021-04-26 14:35:07 +0900

    wildcard

---
+++ src/p13/textbook/s130501/BoundedTypeParameterEcample.java
@@ -0,0 +1,13 @@
+package p13.textbook.s130501;
+
+public class BoundedTypeParameterEcample {
+	public static void main(String[] args) {
+//		Util.compare("a", "b");
+		
+		int result1 = Util.compare(10, 20);
+		System.out.println(result1);
+		
+		int result2 = Util.compare(4.5, 3);
+		System.out.println(result2);
+	}
+}
