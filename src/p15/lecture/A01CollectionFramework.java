d3417beaed031f4f84ca67dcf06dd74f9d3b8230
sebaek <sebaek@gmail.com>
2021-04-27 10:32:13 +0900

    list

---
+++ src/p15/lecture/A01CollectionFramework.java
@@ -0,0 +1,25 @@
+package p15.lecture;
+
+public class A01CollectionFramework {
+	// collection : 인스턴스를 저장한 인스턴스
+	
+	// data를 저장 또는 관리하는 방법 : 자료구조(data structure)
+	
+	// collection framework
+	// 자주 사용하는 자료구조에 관련된
+	// 클래스와 인터페이스, 메소드들 묶음
+	
+	// 대표인터페이스
+	// List (순서 있고, 중복 저장 가능)
+	// Set (순서 없고, 중복 불가)
+	// Map (키,값) 쌍으로 저장
+}
+
+
+
+
+
+
+
+
+
