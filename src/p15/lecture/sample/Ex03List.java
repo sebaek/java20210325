36e2ff9eb359618cf9dd66f1c567860632790625
sebaek <sebaek@gmail.com>
2021-04-27 14:45:02 +0900

    merge

--- src/p15/lecture/sample/Ex03List.java
+++ src/p15/lecture/sample/Ex03List.java
@@ -6,11 +6,36 @@ import java.util.List;
 
 public class Ex03List {
 	public static void main(String[] args) {
-		List<Integer> list1 = Arrays.asList(7,3,5,1);
-		List<Integer> list2 = Arrays.asList(3,2,3,5,0,2);
+		List<Integer> list1 = Arrays.asList(7,3,5,1,0,2);
+		List<Integer> list2 = Arrays.asList(3,2,3,5);
 		
 		List<Integer> merge1 = merge1(list1, list2);
 		System.out.println(merge1);
+		
+		List<Integer> merge2 = merge2(list1, list2);
+		System.out.println(merge2);
+	}
+
+	private static List<Integer> merge2(List<Integer> list1, List<Integer> list2) {
+		List<Integer> merge = new ArrayList<>();
+		int i1 = list1.size() - 1;
+		int i2 = list2.size() - 1;
+		
+		while (i1 >= 0 || i2 >= 0) {
+			if (i1 >= 0) {
+				merge.add(list1.get(i1));
+			}
+
+			if (i2 >= 0) {
+				merge.add(list2.get(i2));
+			}
+
+			i1--;
+			i2--;
+		}
+		
+		
+		return merge;
 	}
 
 	private static List<Integer> merge1(List<Integer> list1, List<Integer> list2) {
