521dd3b1de6bf11d14af060b78f735017c8ba9b0
sebaek <sebaek@gmail.com>
2021-04-27 11:38:18 +0900

    max index

--- src/p15/lecture/sample/Ex01List.java
+++ src/p15/lecture/sample/Ex01List.java
@@ -19,6 +19,20 @@ public class Ex01List {
 		System.out.println(list.get(indexOfMax));
 	}
 	
+	private static int indexOfMax(List<Integer> list) {
+		int index = 0;
+		int max = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < list.size(); i++) {
+			if (max < list.get(i)) {
+				max = list.get(i);
+				index = i;
+			}
+		}
+		
+		return index;
+	}
+
 	private static int max(List<Integer> list) {
 		int max = Integer.MIN_VALUE;
 		
