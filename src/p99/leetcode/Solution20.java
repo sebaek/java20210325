264d345af0be77398adc3d38b92efad91b5b5e58
sebaek <sebaek@gmail.com>
2021-04-28 14:46:51 +0900

    solution 20

---
+++ src/p99/leetcode/Solution20.java
@@ -0,0 +1,41 @@
+package p99.leetcode;
+
+public class Solution20 {
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+
+			if (c == ')') {
+				if (stack.empty()) {
+					return false;
+				}
+				char o = stack.pop();
+				if (o != '(') {
+					return false;
+				}
+			} else if (c == '}') {
+				if (stack.empty()) {
+					return false;
+				}
+				char o = stack.pop();
+				if (o != '{') {
+					return false;
+				}
+			} else if (c == ']') {
+				if (stack.empty()) {
+					return false;
+				}
+				char o = stack.pop();
+				if (o != '[') {
+					return false;
+				}
+			} else {
+				stack.push(c);
+			}
+		}
+
+		return stack.empty();
+	}
+}
