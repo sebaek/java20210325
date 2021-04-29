c47ddf267e5dbad43704d7a339ab9a64398e9b73
sebaek <sebaek@gmail.com>
2021-04-29 12:16:20 +0900

    ex 07

---
+++ src/p15/lecture/sample/Ex07Map.java
@@ -0,0 +1,23 @@
+package p15.lecture.sample;
+
+import java.util.Map;
+
+public class Ex07Map {
+	public static void main(String[] args) {
+		int[] arr1 = { 1, 1, 1, 3, 3, 2, 3, 2, 1, 4, 5, 5 };
+		
+		Map<Integer, Integer> map = count(arr1);
+
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
+	}
+}
+
+
+
+
+
+
+
+
