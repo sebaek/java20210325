da25041744d8fb5da7493a60528108ffe1f0d58b
sebaek <sebaek@gmail.com>
2021-04-29 10:11:38 +0900

    sum

---
+++ src/p15/lecture/sample/Ex06Set.java
@@ -0,0 +1,18 @@
+package p15.lecture.sample;
+
+public class Ex06Set {
+	public static void main(String[] args) {
+		int[] arr1 = {1, 2, 3, 4, 5, 2, 3};
+		int[] arr2 = {1, 2, 3, 4};
+		
+		int sum1 = sum(arr1);
+		int sum2 = sum(arr2);
+		
+		System.out.println(sum1);
+		System.out.println(sum2);
+	}
+	
+	public static int sum(int[] nums) {
+		
+	}
+}
