cde0bc88d844894bc7e530703c6564f14a336c5d
sebaek <sebaek@gmail.com>
2021-04-30 15:29:28 +0900

    comparable

---
+++ src/p15/textbook/exercises/ex10/Student.java
@@ -0,0 +1,20 @@
+package p15.textbook.exercises.ex10;
+
+public class Student implements Comparable<Student> {
+	public String id;
+	public int score;
+
+	public Student(String id, int score) {
+		this.id = id;
+		this.score = score;
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		return this.score - o.score;
+	}
+}
+
+
+
+
