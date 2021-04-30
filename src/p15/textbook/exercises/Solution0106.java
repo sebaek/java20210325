c6e8f20769105cf62e82d3bf95fa8f0cb7d1da18
sebaek <sebaek@gmail.com>
2021-04-30 11:14:30 +0900

    solution

---
+++ src/p15/textbook/exercises/Solution0106.java
@@ -0,0 +1,53 @@
+package p15.textbook.exercises;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import p15.textbook.Board;
+
+public class Solution0106 {
+	// 1번 문제
+	// 답: 4 (Stack == LIFO, Queue == FIFO)
+	
+	// 2번문제
+	// 답: 3
+	
+	// 3번문제
+	// 답: 4
+	
+	// 4번문제
+	// 답: 3
+	
+	// 5번문제
+	List<Board> list = new ArrayList<>();
+	
+	// 6번문제
+	Map<String, Integer> map = new HashMap<>(); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/p15/textbook/exercises/ex09/MapExample.java
@@ -0,0 +1,42 @@
+package p15.textbook.exercises.ex09;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<>();
+		
+		map.put("blue", 96);
+		map.put("hong", 86);
+		map.put("white", 92);
+		
+		String name = null;
+		int maxScore = 0;
+		int totalScore = 0;
+		
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			String key = entry.getKey();
+			int value = entry.getValue();
+			
+			if (value > maxScore) {
+				name = key;
+				maxScore = value;
+			}
+			
+			totalScore += value;
+		}
+		
+		System.out.println("평균점수: " + ((double)totalScore / map.size()));
+		System.out.println("최고점수: " + maxScore);
+		System.out.println("최고점수를 받은 아이디: " + name);
+	}
+}
+
+
+
+
+
+
+
+
