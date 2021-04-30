4d77e2ac6b3ad736d0fa4cc2f1b2424b84191427
sebaek <sebaek@gmail.com>
2021-04-30 12:11:44 +0900

    read file

--- src/p15/lecture/A13Properties.java
+++ src/p15/lecture/A13Properties.java
@@ -1,5 +1,37 @@
 package p15.lecture;
 
-public class A13Properties {
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class A13Properties {
+	public static void main(String[] args) throws Exception {
+		Map<String, String> map1 = new HashMap<>();
+		
+		String path = "bin/p15/lecture/config1.properties";
+		Scanner scanner = new Scanner(new File(path));
+		
+		while (scanner.hasNextLine()) {
+			System.out.println(scanner.nextLine());
+		}
+		
+		scanner.close();
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
