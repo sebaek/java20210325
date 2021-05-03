f96cd5707edaf505cc015d4b793b3f72728f3e22
sebaek <sebaek@gmail.com>
2021-05-03 14:33:00 +0900

    write line

--- src/p18/lecture/A14ToTextFile.java
+++ src/p18/lecture/A14ToTextFile.java
@@ -1,11 +1,15 @@
 package p18.lecture;
 
+import java.io.FileWriter;
+import java.io.Writer;
 import java.util.Scanner;
 
 public class A14ToTextFile {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		String file = "src/p18/lecture/output4.txt";
 		Scanner scanner = new Scanner(System.in);
+		
+		Writer wr = new FileWriter(file);
 		
 		String line = "";
 		
@@ -14,9 +18,11 @@ public class A14ToTextFile {
 			line = scanner.nextLine();
 			// line이 저장한 문자열을 output4.txt에 쓰기
 			// write시 끝에 enter 추가 "\n"
+			wr.write(line + "\n");
 			
 		}
 		
+		wr.close();
 		scanner.close();
 		
 	}
