f95ff94f55847fe8533ce58831f1eb232c1d813b
sebaek <sebaek@gmail.com>
2021-05-03 12:12:29 +0900

    copy file

---
+++ src/p18/lecture/A12FileCopy.java
@@ -0,0 +1,29 @@
+package p18.lecture;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+public class A12FileCopy {
+	public static void main(String[] args) throws Exception {
+		// 파일 복사 Reader, Writer 이용
+		
+		String src = "src/p18/lecture/A12FileCopy.java";
+		String des = "src/p18/lecture/A12FileCopy.copy";
+		
+		Reader fr = new FileReader(src);
+		Writer fw = new FileWriter(des);
+		
+		// 코드 작성......
+		
+		fr.close();
+		fw.close();
+		
+		System.out.println("복사 완료");
+	}
+}
+
+
+
+
