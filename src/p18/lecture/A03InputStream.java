dacd769e82c8154e8c3aabde5ee2fc1b0fd1ea48
sebaek <sebaek@gmail.com>
2021-05-03 10:05:54 +0900

    inputstream

--- src/p18/lecture/A03InputStream.java
+++ src/p18/lecture/A03InputStream.java
@@ -7,20 +7,28 @@ public class A03InputStream {
 	public static void main(String[] args) throws Exception {
 		String fileName = "src/p18/lecture/A02InputStream.java";
 		InputStream is = new FileInputStream(fileName);
-		
+
 		byte[] datas = new byte[20];
-		
-		int cnt = is.read(datas);
-		
-		System.out.println(datas[0]);
-		System.out.println(datas[1]);
-		System.out.println(datas[2]);
-		
-		System.out.println(cnt);
-		
+
+//		int cnt = is.read(datas);
+//		
+//		System.out.println(datas[0]);
+//		System.out.println(datas[1]);
+//		System.out.println(datas[2]);
+//		
+//		System.out.println(cnt);
+
+		int cnt = 0;
+		while ((cnt = is.read(datas)) != -1) {
+			System.out.println(cnt);
+		}
+
 		is.close();
 	}
 }
+
+
+
 
 
 
