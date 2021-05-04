d75a1425d2ee7971294982ba0f9b5070c52a9fca
sebaek <sebaek@gmail.com>
2021-05-04 15:55:48 +0900

    server

--- src/p18/textbook/exercises/ex11/ServerExample.java
+++ src/p18/textbook/exercises/ex11/ServerExample.java
@@ -10,34 +10,38 @@ public class ServerExample {
 	public static void main(String[] args) throws Exception {
 		ServerSocket serverSocket = new ServerSocket();
 		serverSocket.bind(new InetSocketAddress("172.30.1.43", 5001));
-		
+
 		System.out.println("[서버 시작]");
-		
+
 		try {
 			Socket socket = serverSocket.accept();
 			InputStream is = socket.getInputStream();
-			
+
 			byte[] bytes = new byte[1000];
 			int readByteCount = -1;
-			
+
 			readByteCount = is.read(bytes, 0, 100);
-		
+
 			String fileName = new String(bytes, 0, readByteCount);
 			fileName = fileName.trim();
-			
+
 			System.out.println("[파일 받기 시작]" + fileName);
 			FileOutputStream fos = new FileOutputStream("/Temp" + fileName);
 			int len = 0;
 			while ((len = is.read(bytes)) != -1) {
 				fos.write(bytes, 0, len);
 			}
-			
-			
+
+			System.out.println("[파일 받기 완료]");
+
 			fos.close();
 			is.close();
 			socket.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			serverSocket.close();
 		}
+		System.out.println("[서버 종료]");
 	}
 }
