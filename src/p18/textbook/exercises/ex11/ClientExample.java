3589f6428d4118fdbfaf5b8b4e4f047fe803e8ce
sebaek <sebaek@gmail.com>
2021-05-04 15:54:42 +0900

    server

---
+++ src/p18/textbook/exercises/ex11/ClientExample.java
@@ -0,0 +1,7 @@
+package p18.textbook.exercises.ex11;
+
+public class ClientExample {
+	public static void main(String[] args) {
+		String path = "src/p18/textbook/exercises/ex11/ClientExample.java";
+	}
+}
---
+++ src/p18/textbook/exercises/ex11/ServerExample.java
@@ -0,0 +1,43 @@
+package p18.textbook.exercises.ex11;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerExample {
+	public static void main(String[] args) throws Exception {
+		ServerSocket serverSocket = new ServerSocket();
+		serverSocket.bind(new InetSocketAddress("172.30.1.43", 5001));
+		
+		System.out.println("[서버 시작]");
+		
+		try {
+			Socket socket = serverSocket.accept();
+			InputStream is = socket.getInputStream();
+			
+			byte[] bytes = new byte[1000];
+			int readByteCount = -1;
+			
+			readByteCount = is.read(bytes, 0, 100);
+		
+			String fileName = new String(bytes, 0, readByteCount);
+			fileName = fileName.trim();
+			
+			System.out.println("[파일 받기 시작]" + fileName);
+			FileOutputStream fos = new FileOutputStream("/Temp" + fileName);
+			int len = 0;
+			while ((len = is.read(bytes)) != -1) {
+				fos.write(bytes, 0, len);
+			}
+			
+			
+			fos.close();
+			is.close();
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
