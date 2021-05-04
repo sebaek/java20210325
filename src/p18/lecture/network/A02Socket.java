75ab62c509690e03406c03170d54bd01bf413488
sebaek <sebaek@gmail.com>
2021-05-04 14:33:54 +0900

    socket

---
+++ src/p18/lecture/network/A02Socket.java
@@ -0,0 +1,39 @@
+package p18.lecture.network;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class A02Socket {
+	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+
+		try {
+			serverSocket = new ServerSocket();
+			serverSocket.bind(new InetSocketAddress("172.30.1.43", 5001));
+
+			System.out.println("[연결 기다림]");
+			socket = serverSocket.accept();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (socket != null) {
+					socket.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (serverSocket != null) {
+					serverSocket.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
