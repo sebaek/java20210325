8aea1a14bcfd80186a4729f8a3c1c88ce03769dd
sebaek <sebaek@gmail.com>
2021-05-04 09:32:20 +0900

    ex07

---
+++ src/p18/textbook/exercises/ex07/AddLineNumberExample.java
@@ -0,0 +1,8 @@
+package p18.textbook.exercises.ex07;
+
+public class AddLineNumberExample {
+	public static void main(String[] args) {
+		String path 
+		= "src/p18/textbook/exercises/ex07/AddLineNumberExample.java";
+	}
+}
