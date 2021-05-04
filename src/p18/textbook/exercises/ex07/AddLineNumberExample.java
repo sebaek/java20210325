f9d1ced8062427a6181f095be1847d4831800f22
sebaek <sebaek@gmail.com>
2021-05-04 10:06:42 +0900

    ex07

--- src/p18/textbook/exercises/ex07/AddLineNumberExample.java
+++ src/p18/textbook/exercises/ex07/AddLineNumberExample.java
@@ -1,8 +1,27 @@
 package p18.textbook.exercises.ex07;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class AddLineNumberExample {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		String path 
 		= "src/p18/textbook/exercises/ex07/AddLineNumberExample.java";
+		FileReader fr = new FileReader(path);
+		BufferedReader br = new BufferedReader(fr);
+		
+		String line = null;
+		int lineNum = 0;
+		while ((line = br.readLine()) != null) {
+			System.out.println((++lineNum) + ": " + line);
+		}
+		
+		br.close();
+		fr.close();
 	}
 }
+
+
+
+
+
