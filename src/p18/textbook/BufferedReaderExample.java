c026348367b2d534cc8900ce6f7159720f5b07f5
sebaek <sebaek@gmail.com>
2021-05-04 09:30:26 +0900

    bufferedreader

---
+++ src/p18/textbook/BufferedReaderExample.java
@@ -0,0 +1,24 @@
+package p18.textbook;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class BufferedReaderExample {
+	public static void main(String[] args) throws Exception {
+		InputStream is = System.in;
+		Reader reader = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(reader);
+		
+		System.out.print("입력:");
+		String lineString = br.readLine();
+		
+		System.out.println("출력:" + lineString);
+		
+		br.close();
+	}
+}
+
+
+
