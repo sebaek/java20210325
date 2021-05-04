e7331d77e0016e55b7d2ab576817a2f23132f76f
sebaek <sebaek@gmail.com>
2021-05-04 14:08:53 +0900

    solution 1~6

---
+++ src/p18/textbook/exercises/ex07/Solution0106.java
@@ -0,0 +1,31 @@
+package p18.textbook.exercises.ex07;
+
+public class Solution0106 {
+	// 1번 문제
+	// 답: 1번
+	
+	// 2번 문제
+	// 답: 1번
+	
+	// 3번 문제
+	// 답: 4번
+	
+	// 4번 문제
+	// 답: 1번
+
+	// 5번 문제
+	// 답: 3번 --> ObjectInputStream, ObjectOutputStream
+	
+	// 6번 문제
+	// 답: 3번
+	
+}
+
+
+
+
+
+
+
+
+
