82af8025d1e17d8d013bd279e277785596a4aa71
sebaek <sebaek@gmail.com>
2021-05-04 10:25:14 +0900

    buffered output stream example

---
+++ src/p18/textbook/BufferedOutputStreamExample.java
@@ -0,0 +1,26 @@
+package p18.textbook;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class BufferedOutputStreamExample {
+	public static void main(String[] args) {
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		BufferedInputStream bis = null;
+		BufferedOutputStream bos = null;
+		
+		int data = -1;
+		long start = 0;
+		long end = 0;
+		
+		String src = "src/p18/textbook/BufferedOutputStreamExample.java";
+		String des = "src/p18/textbook/BufferedOutputStreamExample.copy";
+	}
+}
+
+
+
+
